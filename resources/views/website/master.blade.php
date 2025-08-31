65f3f78ab618aca42c636fc2bfd8aeba29e02196
asraf705 <asrafuzzaman705@gmail.com>
2025-08-31 14:29:26 +0600

    changed 7 files

--- resources/views/website/master.blade.php
+++ resources/views/website/master.blade.php
@@ -714,13 +714,13 @@
     <!-- ===============================================-->
     <!--    JavaScripts-->
     <!-- ===============================================-->
-    <script src="vendors/@popperjs/popper.min.js"></script>
-    <script src="vendors/bootstrap/bootstrap.min.js"></script>
-    <script src="vendors/is/is.min.js"></script>
+    <script src="{{ asset('website/vendors/@popperjs/popper.min.js') }}"></script>
+    <script src="{{ asset('website/vendors/bootstrap/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('website/vendors/is/is.min.js') }}"></script>
     <script src="https://scripts.sirv.com/sirvjs/v3/sirv.js"></script>
     <script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
-    <script src="vendors/fontawesome/all.min.js"></script>
-    <script src="assets/js/theme.js"></script>
+    <script src="{{ asset('website/vendors/fontawesome/all.min.js') }}"></script>
+    <script src="{{ asset('website/assets/js/theme.js') }}"></script>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
