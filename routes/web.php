3c5727f131378c074e6515403d9386216bc51b51
asraf705 <asrafuzzaman705@gmail.com>
2025-08-31 14:26:48 +0600

    changed 96 files

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Website\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
 
 Auth::routes();
 
