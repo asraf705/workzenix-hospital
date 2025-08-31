a82cef9533c127cb30e1637853a470e923435a3f
asraf705 <asrafuzzaman705@gmail.com>
2025-08-31 18:12:29 +0600

    modified HomeController.php added index.blade.php

--- app/Http/Controllers/Website/HomeController.php
+++ app/Http/Controllers/Website/HomeController.php
@@ -11,4 +11,9 @@ class HomeController extends Controller
     {
         return view('website.pages.home.index');
     }
+
+    public function about()
+    {
+        return view('website.pages.about.index');
+    }
 }
